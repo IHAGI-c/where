1ff6a8e27e0ff1f6da6124958794265cc7eab2a2
IHAGI-c <ejrgud31@naver.com>
2021-08-27 15:46:46 +0900

    where finish

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from accountapp.views import hello_world, about_us, album, main_
+from accountapp.views import about_us, album, main_
 
 app_name = "accountapp"
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world'),
     path('about_us/', about_us, name='about_us'),
     path('albums/', album, name='album'),
     path('main/', main_, name='main'),
--- where/urls.py
+++ where/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from accountapp.views import main_
+
 urlpatterns = [
+    path('', main_, name='home'),
+
     path('admin/', admin.site.urls),
     path('account/', include('accountapp.urls')),
     path('articles/', include('articleapp.urls')),
